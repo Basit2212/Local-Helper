3618d448fa94fbb2b4841d6a9d7bd9c7902f6547
Basit Khan <abdulbasitjunaidahmed17@gmail.com>
2025-03-12 14:20:17 +0500

    Add new changes

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
-import './../../Style/Footer.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import './../../assets/Style/Footer.css'
+
+const icons = [
+  {
+    Image: <i className="bi bi-geo-alt-fill"></i>,
+    text: "lorem ipsum dummy text address"
+  },
+  {
+    Image: <i className="bi bi-telephone-fill"></i>,
+    text: "92 1234567890"
+  },
+  {
+    Image: <i className="bi bi-envelope-fill"></i>,
+    text: "demo@gmail.com"
+  }
+]
 
 const Footer = () => {
   return (
-    <>
-      <section>
-        <Container>
-          <Row>
-            <h4>Get in touch</h4>
-          </Row>
-        </Container>
-      </section>
-    </>
+    <section className='footer-section'>
+      <Container>
+        <h4>Get in touch</h4>
+        <Row>
+          {icons.map((item, index) => (
+            <Col  key={index} md={4} className="footer-item mx-auto">
+              <div className="footerImg">
+                {item.Image}
+              </div>
+              <p>{item.text}</p>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
   )
 }
 
