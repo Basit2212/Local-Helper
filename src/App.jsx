81e596b8a7b05216795484844cb04a30ffd2710d
Basit Khan <abdulbasitjunaidahmed17@gmail.com>
2025-03-18 17:37:46 +0500

    Fix React Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from './assets/Pages/Home';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './assets/Pages/Sections/About';
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
 import OurServices from './assets/Pages/Sections/OurServices';
 import ContactUs from './assets/Pages/Sections/ContactUs';
 
-
-
-
 function App() {
- 
-
   return (
     <>
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/Home' element={<Home />}/>
-        <Route path='/About' element ={ <About />   }/>
-        <Route path='/Services' element ={ <OurServices />   }/>
-        <Route path='/Contact' element ={<ContactUs />    }/>
-      </Routes>
-      <Footer/>
-    </Router>
-    
-      </>
-  )
+      <Router basename="/Local-Helper">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Services" element={<OurServices />} />
+          <Route path="/Contact" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
